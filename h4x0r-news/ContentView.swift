b0da30fbf6148c1a56a3304caec0e6dd0c82a446
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-12-24 17:18:39 +0530

    Initial Commit

---
+++ h4x0r-news/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  h4x0r-news
+//
+//  Created by Saurabh Mirajkar on 24/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ h4x0r-news/h4x0r_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  h4x0r_newsApp.swift
+//  h4x0r-news
+//
+//  Created by Saurabh Mirajkar on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct h4x0r_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
